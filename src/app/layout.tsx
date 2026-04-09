b0581a466c630fb786b0a775e7d60121f975bab1
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-04-09 17:27:48 -0300

    add QueryProvider component for React Query integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import QueryProvider from "@/components/providers/queryProvider";
 import type { Metadata } from "next";
 import { Catamaran } from "next/font/google";
 import "./globals.css";
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${catamaran.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
