84f46d8fa6af1ba212f2e4fc9fba294bb5bf5853
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-04-11 23:58:16 -0300

    Create home page navigation link

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,9 +1,32 @@
 "use client";
-import { useRouter } from "next/navigation";
+import Text from "@/components/text";
+import { routes } from "@/lib/routes";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+import { use } from "react";
+interface Props {
+  params: Promise<{ id: string }>;
+}
 
-const MovieDetails = () => {
-  const router = useRouter();
-  return <button onClick={() => router.back()} className="text-gray-100">click me</button>;
+const MovieDetails = ({ params }: Props) => {
+  const { id } = use(params);
+
+  return (
+    <main>
+      <Link
+        href={routes.home}
+        className="flex flex-nowrap gap-1 items-center ml-3 group"
+      >
+        <ChevronLeft className="text-gray-100 group-hover:text-yellow-light transition group-hover:animate-bounce-x -mt-1" />
+        <Text
+          variant="body-md"
+          className="transition border-b border-transparent group-hover:border-yellow group-hover:text-yellow-light"
+        >
+          Back to Home
+        </Text>
+      </Link>
+    </main>
+  );
 };
 
 export default MovieDetails;
