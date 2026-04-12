91b7ddac1c114b937b2ffa90cee6507bfde3b080
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-04-12 00:23:33 -0300

    Replace link with button for router back navigation

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,30 +1,31 @@
 "use client";
 import Text from "@/components/text";
-import { routes } from "@/lib/routes";
 import { ChevronLeft } from "lucide-react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { use } from "react";
+
 interface Props {
   params: Promise<{ id: string }>;
 }
 
 const MovieDetails = ({ params }: Props) => {
   const { id } = use(params);
+  const router = useRouter();
 
   return (
     <main>
-      <Link
-        href={routes.home}
-        className="flex flex-nowrap gap-1 items-center ml-3 group"
+      <button
+        onClick={() => router.back()}
+        className="flex flex-nowrap gap-1 items-center ml-3 group cursor-pointer"
       >
         <ChevronLeft className="text-gray-100 group-hover:text-yellow-light transition group-hover:animate-bounce-x -mt-1" />
         <Text
           variant="body-md"
           className="transition border-b border-transparent group-hover:border-yellow group-hover:text-yellow-light"
         >
-          Back to Home
+          Back
         </Text>
-      </Link>
+      </button>
     </main>
   );
 };
